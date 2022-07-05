8688bfb7c5362ae508a2ab4ef115d4e6e999fd9e
Cristiano Gonçalves <cprimbee@gmail.com>
2022-07-05 09:16:26 -0300

    upload start project

---
+++ bin/base_backend.dart
@@ -0,0 +1,5 @@
+import 'package:base_backend/base_backend.dart' as base_backend;
+
+void main(List<String> arguments) {
+  print('Hello world: ${base_backend.calculate()}!');
+}
---
+++ test/base_backend_test.dart
@@ -0,0 +1,8 @@
+import 'package:base_backend/base_backend.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
